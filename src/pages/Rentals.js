d76d7acb82cdb6d81941b310b50695965e504e96
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-04-26 16:08:16 +0200

    $0011: [RentalPage]: search reminder filters

--- src/pages/Rentals.js
+++ src/pages/Rentals.js
@@ -3,7 +3,7 @@ import "./Rentals.css";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router";
 import logo from "../images/airbnbRed.png";
-import { ConnectButton } from "web3uikit";
+import { ConnectButton, Icon } from "web3uikit";
 
 const Rentals = () => {
 
@@ -17,7 +17,33 @@ const Rentals = () => {
           <img className="logo" src={logo} alt="logo"></img>  
         </Link>
       </div>
-      <div className="searchReminder"></div>
+      <div className="searchReminder">
+        <div className="filter">
+          {
+            searchFilters.destination
+          }
+        </div>
+        <div className="vl" />
+      
+        <div className="filter">
+          {`
+            ${searchFilters.checkIn.toLocaleString("default", {month: "short",})}
+            ${searchFilters.checkIn.toLocaleString("default", {day: "2-digit",})}
+            -
+            ${searchFilters.checkOut.toLocaleString("default", {month: "short",})}
+            ${searchFilters.checkOut.toLocaleString("default", {day: "2-digit",})}
+          `}
+        </div>
+        <div className="vl" />
+
+        <div className="filter">
+          { searchFilters.guests } Guest
+        </div>
+
+        <div className="searchFiltersIcon">
+          <Icon fill="#ffffff" size={20} svg="search" />
+        </div>
+      </div>
       <div className="lrContainers">
         <ConnectButton />
       </div>
