1b3049280a222f40a73355f73e9ccba2c90d1d3d
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-04-26 13:55:51 +0200

    $0010: [RentalPage]: simple rental page layout

--- src/pages/Rentals.js
+++ src/pages/Rentals.js
@@ -2,6 +2,8 @@ import React from "react";
 import "./Rentals.css";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router";
+import logo from "../images/airbnbRed.png";
+import { ConnectButton } from "web3uikit";
 
 const Rentals = () => {
 
@@ -9,12 +11,17 @@ const Rentals = () => {
 
   return (
     <>
-     <Link to="/"> Home </Link>
-      {searchFilters && 
-      <div onClick={() => console.log(searchFilters)}>
-         click
+    <div className="topBanner">
+      <div>
+        <Link to="/">
+          <img className="logo" src={logo} alt="logo"></img>  
+        </Link>
       </div>
-      }
+      <div className="searchReminder"></div>
+      <div className="lrContainers">
+        <ConnectButton />
+      </div>
+    </div>
     </>
   );
 };
