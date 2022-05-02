939fa3a038ecd7b9c16a2e45278561f62ab16639
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-02 16:36:13 +0200

    $0019: [Rentals]: add rental objects for 2 additional cities

--- src/sampleRentalsList.js
+++ src/sampleRentalsList.js
@@ -18,15 +18,15 @@
 [
     {
       attributes: {
-        city: "Los Angeles",
-        unoDescription: "2 Guests • 2 Beds • 1 Rooms",
-        dosDescription: "Wifi • Kitchen • Living Area",
+        city: "New York",
+        unoDescription: "1 Guests • 1 Beds",
+        dosDescription: "Wifi • Self Check In • Work Space",
         imgUrl:
-          "https://ipfs.moralis.io:2053/ipfs/QmS3gdXVcjM72JSGH82ZEvu4D7nS6sYhbi5YyCw8u8z4pE/media/3",
-        lat: "40.716862",
-        long: "-73.999005",
-        name: "Apartment in Los Angeles city Center",
-        pricePerDay: "10",
+          "https://ipfs.moralis.io:2053/ipfs/QmS3gdXVcjM72JSGH82ZEvu4D7nS6sYhbi5YyCw8u8z4pE/media/6",
+        lat: "40.767589",
+        long: "-73.969231",
+        name: "Studio in Central Park",
+        pricePerDay: "1",
       },
     },
   ];
@@ -34,15 +34,15 @@
   [
     {
       attributes: {
-        city: "Dubai",
-        unoDescription: "1 Guests • 2 Beds • 5 Rooms",
-        dosDescription: "Wifi • Kitchen • Living Area • Bitcoin ATM",
+        city: "London",
+        unoDescription: "2 Guests • 1 Beds • 1 Bath",
+        dosDescription: "Wifi • Work Station",
         imgUrl:
-          "https://ipfs.moralis.io:2053/ipfs/QmS3gdXVcjM72JSGH82ZEvu4D7nS6sYhbi5YyCw8u8z4pE/media/3",
-        lat: "40.716862",
-        long: "-73.999005",
-        name: "Luxery Apartment in Dubai",
-        pricePerDay: "1000",
+          "https://ipfs.moralis.io:2053/ipfs/QmS3gdXVcjM72JSGH82ZEvu4D7nS6sYhbi5YyCw8u8z4pE/media/7",
+        lat: "51.500075",
+        long: "-0.125706",
+        name: "Tropical Ambiance in London",
+        pricePerDay: "1",
       },
     },
   ]; 
