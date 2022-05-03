7fdbf91ecf1fedeaa3ab085c09b7f31b47b070b4
gepthecoder <38008294+gepthecoder@users.noreply.github.com>
2022-05-03 14:07:08 +0200

    $0025: [EndDisplay]: users booked rentals with description and img (wraped in card) - Part2

--- src/components/User.js
+++ src/components/User.js
@@ -1,12 +1,30 @@
 import React from "react";
-import { Icon, Modal } from "web3uikit";
-import { useState } from "react";
-
+import { Icon, Modal, Card } from "web3uikit";
+import { useState, useEffect } from "react";
+import { useMoralis } from "react-moralis";
 
 function User({account}) {
 
-
   const [isVisible, setVisible] = useState(false);
+  const { Moralis } = useMoralis();
+  const [userRentals, setUserRentals] = useState();
+
+  // runs whenever that isVisible variable state changes 
+  useEffect(
+    () => {
+      // fetch data from Moralis db for users booked rentals
+      async function fetchRentals() {
+        const Rentals = Moralis.Object.extend("newBookings");
+        const query = new Moralis.Query(Rentals);
+        query.equalTo("booker", account);
+        const result = await query.find();
+
+        setUserRentals(result);
+      }
+
+      fetchRentals();
+
+    }, [isVisible]);
 
   return (
     <>
@@ -20,6 +38,33 @@ function User({account}) {
         title = "Your Booked Stays"
         isVisible = {isVisible}
       >
+
+      <div style={{display:"flex", justifyContent:"start", flexWrap:"wrap", gap:"10px"}}>
+         {userRentals &&
+          userRentals.map((e)=>{
+            return(
+              <div style={{ width: "200px" }}>
+                <Card
+                  isDisabled
+                  title={e.attributes.city}
+                  description={`${e.attributes.datesBooked[0]} for ${e.attributes.datesBooked.length} Days`}
+                >
+                  <div>
+                    <img
+                      width="180px"
+                      src={e.attributes.imgUrl}
+                    />
+                  </div>
+                </Card>
+              </div>
+            )
+          })
+
+         }
+        </div>
+
+
+
       </Modal>
 
     </>
